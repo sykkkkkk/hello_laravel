4c3847383703814dc5164dc27838842fc3b91eca
Syk <923703909@qq.com>
2019-07-19 09:39:06 +0000

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,7 +37,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - by Syk</div>
+                <div class="title">Hello Laravel! - by github</div>
             </div>
         </div>
     </body>
